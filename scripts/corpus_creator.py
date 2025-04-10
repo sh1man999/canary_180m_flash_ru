f7ccdc522f42520cc2e3b4fc0a5c012a7c29d998
sh1man <laktapok12@gmail.com>
2025-04-11 02:34:49 +0300

    upd

--- scripts/corpus_creator.py
+++ scripts/corpus_creator.py
@@ -8,7 +8,7 @@ def main():
     corpus_dir = os.path.join(BASE_DIR, "corpus")
     os.makedirs(corpus_dir, exist_ok=True)
     manifest_path = os.path.join(os.path.join(BASE_DIR, 'datasets'))
-    manifest_files = glob.glob(os.path.join(manifest_path, '*.json'))
+    manifest_files = glob.glob(os.path.join(manifest_path, '*.jsonl'))
     with open(os.path.join(corpus_dir, 'russian_corpus.txt'), "w", encoding="utf-8") as f:
         for manifest_file in manifest_files:
             with open(manifest_file) as reader:
